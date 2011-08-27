16d9d753337f846337c16dfb1763b233178ababd
Josep M. Bach <josep.m.bach@gmail.com>
2011-08-27 22:27:13 +0200

    async testing is hard

--- lib/domodoro/client.rb
+++ lib/domodoro/client.rb
@@ -1,21 +1,39 @@
 module Domodoro
   class Client
     class << self
+
       def start(host, port)
         EM.run do
+
           EM.connect(host, port) do |c|
             c.extend EM::P::LineText2
             def c.receive_line(line)
-              puts "[#{Time.now}]: " + line
-              if line =~ /START/ || line =~ /STOP/
-                fork do
-                  system('afplay alarm.mp3 && afplay alarm.mp3 && afplay alarm.mp3')
+              case line
+                when /start/
+                  Client.work
+                when /stop/
+                  Client.break
                 end
               end
             end
           end
+
         end
       end
-    end
+
+      def work
+        puts "WORK!!!!!"
+        fork do
+          system('afplay alarm.mp3 && afplay alarm.mp3')
+        end
+      end
+
+      def break
+        puts "BREAK!!!!!"
+        fork do
+          system('afplay alarm.mp3 && afplay alarm.mp3')
+        end
+      end
+
   end
 end
---
+++ test/domodoro/client_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+module ExampleServer; end
+
+module Received
+  attr_accessor :work, :break
+  extend self
+end
+
+module Domodoro
+  describe Client do
+    include EM::MiniTest::Spec
+
+    before do
+      Received.break = false
+      Received.work = false
+    end
+
+    it 'calls .work when receives :start', :timeout => 0.2 do
+      EM.start_server '127.0.0.1', 12345, ExampleServer do |conn|
+        conn.send_data ":start\n"
+      end
+
+      Client.start '127.0.0.1', 12345
+
+      def Client.work
+        Received.work = true
+      end
+
+      EM.add_timer(0.1) do
+        assert Received.work
+        done!
+      end
+
+      wait!
+    end
+
+    it 'calls .break when receives :stop', :timeout => 0.2 do
+      EM.run do
+      EM.start_server '127.0.0.1', 12345, ExampleServer do |conn|
+        conn.send_data ":stop\n"
+      end
+
+      Client.start '127.0.0.1', 12345
+
+      def Client.break
+        Received.break = true
+      end
+
+      EM.add_timer(0.1) do
+        assert Received.break
+        done!
+      end
+
+      wait!
+      end
+    end
+  end
+end
